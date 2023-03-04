6b19441de7aa8fc1ec22e7ed80e53778157dac77
lawrence-laz <laurynas@lazauskas.info>
2023-03-05 00:40:27 +0200

    chore: Add empty projects

---
+++ src/GenericPipeline/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace GenericPipeline;
+public class Class1
+{
+
+}
---
+++ test/GenericPipeline.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace GenericPipeline.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
