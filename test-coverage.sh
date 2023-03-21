c49aa655c48290ed603d4b75052e2c0812479234
lawrence-laz <laurynas@lazauskas.info>
2023-03-21 22:52:56 +0200

    test: Add test-coverage.sh for local coverage checking

---
+++ test-coverage.sh
@@ -0,0 +1,7 @@
+dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=opencover
+reportgenerator \
+    -reports:"test/GenericPipeline.Tests/TestResults/coverage.opencover.xml" \
+    -targetdir:"coveragereport" \
+    -reporttypes:Html
+xdg-open coveragereport/index.htm
+
